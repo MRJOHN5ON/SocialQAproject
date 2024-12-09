ed444278e0a5b0ede94c8d6a9ef48fcd42643f5a
Ryley Johnson <ryleyjohnsonemail@gmail.com>
2024-12-08 16:27:20 -0800

    debugging timeout issues

--- tests/amazon.spec.ts
+++ tests/amazon.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 import {searchForProductClick, searchForProductEnter} from './helper.ts';
-import { time } from 'console';
 
-test('product page has correct title' , async ({ page }) => {
+
+test('product page should have correct title' , async ({ page }) => {
     await page.goto('https://www.amazon.com/Enjoi-Rasta-Panda-Skateboard-Deck/dp/B07R3GNBZ6/ref=sr_1_4?crid=RJ5HFV9E2QKA&dib=eyJ2IjoiMSJ9.DhWGtwchpml_AsphYaW-nYaQf4BDC7Xnn1offZVR7ceRdujZLcawYUDpgaT5mWG_2YTw__EXNQ-3F0q71tD0aFGdb7HBS3j10YhkOF--Xo9OvVVy21ly8N-Shw8niJe_-xQeKketkoLt_yfTkn0zxUNROy5FRGWj6Bi7zBVSDgAQVb33RZmcifkt-MV4EW2KaInsYq87365EPq52v7OLwVNEG1uFkqvgKDOP_1b1LpdLZJeQYWfohp6zeYAGdUI9fPRhJvEB0-5wuz25GuyvDCg0BmudT7ZwRZfA_xe4En0.zjdRWFeO6bk7rhZIQn3D32Xtds-VcVXmxEHkMdjotBE&dib_tag=se&keywords=enjoi%2Bskateboard&qid=1733001881&sprefix=enjoi%2Bskateboard%2Caps%2C148&sr=8-4&th=1&psc=1');
     const productTitle = page.getByTestId('titleSection')
     await expect(productTitle).toHaveText('        Enjoi Rasta Panda R7 Skateboard Deck       ')
@@ -27,7 +27,7 @@ test('searching a product by text and submit using enter button', async ({page})
     await expect(productTitle).toHaveText('        Enjoi Rasta Panda R7 Skateboard Deck       ')    
 });
 
-test('searching for product by text and clicking search button', async ({page})=> {
+test('searching for product by text and submit by clicking search button', async ({page})=> {
 
     // search for product
     await searchForProductClick(page, 'enjoi skateboard deck')
@@ -73,7 +73,7 @@ test('can find product using ASIN', async ({ page }) => {
     await page.getByRole('link', { name: 'Rasta Panda R7 Skateboard Deck' }).first().click()
     const productTitle = page.getByTestId('titleSection')
     await expect(productTitle).toHaveText('        Enjoi Rasta Panda R7 Skateboard Deck       ')
-    await page.waitForLoadState()
+    await page.waitForLoadState('domcontentloaded')
 
     const asin = page.getByTestId("productDetails_detailBullets_sections1").getByText("ASIN").locator("xpath=following-sibling::*");
     const asinText = await asin.textContent() as string;
